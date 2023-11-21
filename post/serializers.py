8cb64f03c8673b4ab67c1a2dac5cfc569c0872fc
Bams <s.bumsu@gmail.com>
2023-11-21 15:26:16 +0900

    [#12] FEAT: post 관련 오류 수정

--- post/serializers.py
+++ post/serializers.py
@@ -3,9 +3,38 @@ from .models import Post, Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author_profile = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
+    dislikes_count = serializers.SerializerMethodField()
+    image = serializers.SerializerMethodField()
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+
+    def get_dislikes_count(self, obj):
+        return obj.dislikes.count()
+
+    def get_author_profile(self, obj):
+        return obj.author.profile.url if obj.author else None
+
+    def get_image(self, obj):
+        if obj.image and hasattr(obj.image, "url"):
+            return obj.image.url
+        return None
+
     class Meta:
         model = Post
-        fields = ("id", "title", "content", "image", "created_at")
+        fields = (
+            "id",
+            "title",
+            "content",
+            "image",
+            "author",
+            "author_profile",
+            "created_at",
+            "likes_count",
+            "dislikes_count",
+        )
 
 
 class CommentSerializer(serializers.ModelSerializer):
